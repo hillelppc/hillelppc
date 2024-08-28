209cd037a95b987e61673b60f736a09b1bcd74eb
hillelppc <dianadanilyuk20@gmail.com>
2024-08-28 17:51:25 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(event){
+    event.preventDefault();
+    alert("Дякуємо за ваш запит! Ми зв'яжемося з вами найближчим часом.");
+});
